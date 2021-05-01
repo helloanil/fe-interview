b092a58d1ae8d59a88d532a7e1cc4a81418ede0f
Anil <aak@hellofresh.com>
2021-05-01 15:01:37 +0200

    Add date-fns and improve design

--- src/components/Merchants.js
+++ src/components/Merchants.js
@@ -1,29 +1,77 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import { FcMoneyTransfer } from "react-icons/fc";
+import { format as formatDate } from "date-fns";
 
 import Collapse from "../ui-library/collapse/Collapse";
 
 import { useFetchMerchantsQuery } from "../queries/merchants";
 
+const ICON_SIZE = 32;
+
+const MerchantHeaderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const MerchantIcon = styled.img`
+  height: ${ICON_SIZE}px;
+  width: ${ICON_SIZE}px;
+`;
+
+const MerchantTitle = styled.span`
+  font-weight: 600;
+`;
+
+const MerchantCollapseContainer = styled.div`
+  margin: 10px;
+`;
+
+const MerchantTransaction = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
 const Merchants = ({ isBill }) => {
   const { data: merchants } = useFetchMerchantsQuery();
 
+  console.debug(navigator.language);
+
+  const renderMerchantHeader = (merchant) => (
+    <MerchantHeaderContainer>
+      <MerchantTitle>{merchant.name}</MerchantTitle>
+      {merchant.iconUrl ? (
+        <MerchantIcon src={merchant.iconUrl} />
+      ) : (
+        <FcMoneyTransfer size={ICON_SIZE} />
+      )}
+    </MerchantHeaderContainer>
+  );
+
   const renderMerchantTransactions = (merchant) =>
     merchant?.transactions?.map((transaction) => (
-      <div>
-        {transaction.amount} - {transaction.date}
-      </div>
+      <MerchantTransaction>
+        <span>{formatDate(new Date(transaction.date), "MMMM do, yyyy")}</span>
+        <span>{transaction.amount}£</span>
+      </MerchantTransaction>
     ));
 
   if (!merchants || merchants.length === 0) return null;
 
+  console.debug(merchants);
+
   return merchants
     ?.filter((merchant) => merchant.isBill === isBill)
     .map((merchant) => (
-      <Collapse
-        header={merchant.name}
-        content={renderMerchantTransactions(merchant)}
-      />
+      <MerchantCollapseContainer>
+        <Collapse
+          header={renderMerchantHeader(merchant)}
+          content={renderMerchantTransactions(merchant)}
+        />
+      </MerchantCollapseContainer>
     ));
 };
 
--- src/ui-library/collapse/Collapse.js
+++ src/ui-library/collapse/Collapse.js
@@ -2,13 +2,30 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+import { FaChevronCircleRight, FaChevronCircleDown } from "react-icons/fa";
+
 const CollapseContainer = styled.div`
-  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #d9d9d9;
+  border-radius: 5px;
 `;
 
 const CollapseHeader = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  user-select: none;
   padding: 10px;
-  border-bottom: 1px solid black;
+  border-bottom: ${(props) =>
+    props.isCollapsed ? "none" : "1px solid #d9d9d9"};
+  background-color: #fafafa;
+`;
+
+const CollapseHeaderIconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
 `;
 
 const CollapseContent = styled.div`
@@ -23,8 +40,14 @@ const Collapse = ({ header, content }) => {
 
   return (
     <CollapseContainer>
-      <CollapseHeader onClick={handleCollapseHeaderClick}>
-        {isCollapsed ? "›" : "⌄"} {header}
+      <CollapseHeader
+        onMouseUp={handleCollapseHeaderClick}
+        isCollapsed={isCollapsed}
+      >
+        <CollapseHeaderIconContainer>
+          {isCollapsed ? <FaChevronCircleRight /> : <FaChevronCircleDown />}
+        </CollapseHeaderIconContainer>
+        {header}
       </CollapseHeader>
       <CollapseContent isCollapsed={isCollapsed}>{content}</CollapseContent>
     </CollapseContainer>
